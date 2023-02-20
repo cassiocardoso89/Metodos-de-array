1d1923a338404fb6828c184f426c5b99e654cdc0
Cassio de Souza Cardoso <cassio.cardoso@sequor.com.br>
2023-02-20 20:38:48 -0300

    aplicando desconto

---
+++ alura_books/app/metodoForEach.js
@@ -0,0 +1,17 @@
+const livrosElement = document.getElementById('livros')
+
+function exibeLivros(listaDeLivros){
+    listaDeLivros.forEach(livro => {
+        livrosElement.innerHTML += `
+        <div class="livro">
+            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}/>
+            <h2 class="livro__titulo>${livro.titulo}</h2>
+            <p class="livro__descricao">${livro.autor}</p>
+            <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
+            <div class="tags">
+                <span class="tag">${livro.categoria}</span>
+            </div>
+        </div>
+        `
+    })
+}
